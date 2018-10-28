2301539fd7177843676b04ad1c3ae5d4ca90ed4b
Christian <leec22@rpi.edu>
2018-10-28 06:52:09 -0400

    Added enemy AI

--- Assets/Scripts/AI.cs
+++ Assets/Scripts/AI.cs
@@ -2,251 +2,94 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody))]
 public class AI : MonoBehaviour {
     // Comparison NPCs
-    public Rigidbody player;
-    public Rigidbody target;
+    private Rigidbody player;
 
-    // For pursue and evade functions
-    public float maxPrediction;
-    public float maxAcceleration;
+    // Adjustable variables
+    [SerializeField]
+    private float maxSpeed;
+    [SerializeField]
+    private float maxAcceleration;
+    [SerializeField]
+    private float targetRadius;
+    [SerializeField]
+    private float slowRadius;
+    [SerializeField]
+    private float timeToTarget;
 
-    // For arrive function
-    public float maxSpeed;
-    public float targetRadiusL;
-    public float slowRadiusL;
-    public float timeToTarget;
-
-    // For Face function
-    public float maxRotation;
-    public float maxAngularAcceleration;
-    public float targetRadiusA;
-    public float slowRadiusA;
-
-    // For wander function
-    public float wanderOffset;
-    public float wanderRadius;
-    public float wanderRate;
-    private float wanderOrientation;
 
     // Holds the path to follow
-    public GameObject[] Path;
-    public int current = 0;
+    [SerializeField]
+    private Vector3[] Path;
+    [SerializeField]
+    private int current = 0;
 
     protected void Start() {
-        player = GetComponent<NPCController>();
+        player = GetComponent<Rigidbody>();
     }
 
-    // Calculate the target to pursue
-    public void Pursue() {
-        Vector3 steering = target.position - player.position;
-        steering.y = 0;
+    private void FixedUpdate() {
+        if (Input.GetKey(KeyCode.Q)) {
+            PathFollow();
+        }
+    }
 
+    // Move towards the target
+    public void Pursue(Vector3 target) {
+        Vector3 steering = target - player.position;
+        steering.y = 0;
         player.AddForce((steering.normalized * maxSpeed) - player.velocity, ForceMode.VelocityChange);
     }
 
-    // Calculate the target to evade
-    public void Evade() {
-        Vector3 steering = player.position - target.position;
+    // Move away from the target
+    public void Evade(Vector3 target) {
+        Vector3 steering = player.position - target;
         steering.y = 0;
-
         player.AddForce((steering.normalized * maxSpeed) - player.velocity, ForceMode.VelocityChange);
     }
-
-    public void Arrive() {
+    
+    // Move towards the target with slow down.
+    public void Arrive(Vector3 target) {
         // Get the direction to the target
-        Vector3 direction = target.position - player.position;
+        Vector3 direction = target - player.position;
         float distance = direction.magnitude;
 
-        // Check if we are there, return no steering
-        if (distance < targetRadiusL) {
+        // If we are there, stop
+        if (distance < targetRadius) {
             player.velocity = Vector3.zero;
-        }
-
-        // If we are outside the slowRadius, then go max speed
-        // Otherwise calculate a scaled speed
-        float targetSpeed = (distance > slowRadiusL ? maxSpeed : maxSpeed * distance / slowRadiusL);
-
-        // Acceleration tries to get to the target velocity
-        Vector3 steering = (direction.normalized * targetSpeed - player.velocity) / timeToTarget;
-
-        // Check if the acceleration is too fast
-        player.AddForce(Vector3.ClampMagnitude(steering, maxAcceleration), ForceMode.VelocityChange);
-    }
-
-    // Calculate the target to face
-    public float FaceAway() {
-        // Work out the direction to target
-        Vector3 direction = character.position - target.position;
-
-        // Check for a zero direction, and make no change if so
-        if (direction.magnitude == 0) {
-            return 0;
-        }
-
-        // Get the naive direction to the target
-        float rotation = Mathf.Atan2(direction.x, direction.z) - character.orientation;
-
-        // Map the result to the (0, 2pi) interval
-        while (rotation > Mathf.PI) {
-            rotation -= 2 * Mathf.PI;
-        }
-        while (rotation < -Mathf.PI) {
-            rotation += 2 * Mathf.PI;
-        }
-        float rotationSize = Mathf.Abs(rotation);
-
-        // Check if we are there, return no steering
-        if (rotationSize < targetRadiusA) {
-            character.rotation = 0;
-        }
-
-        // If we are outside the slowRadius, then use max rotation
-        // Otherwise calculate a scaled rotation
-        float targetRotation = (rotationSize > slowRadiusA ? maxRotation : maxRotation * rotationSize / slowRadiusA);
-
-        // The final target rotation combines speed (already in the variable) and direction
-        targetRotation *= rotation / rotationSize;
-
-        // Acceleration tries to get to the target rotation
-        float angular = targetRotation - character.rotation;
-        angular /= timeToTarget;
-
-        // Check if the acceleration is too great
-        float angularAcceleration = Mathf.Abs(angular);
-        if (angularAcceleration > maxAngularAcceleration) {
-            angular /= angularAcceleration;
-            angular *= maxAngularAcceleration;
-        }
-
-        return angular;
-    }
-
-    // Calculate the target to face
-    public float Face() {
-        var targetDir = sphere.transform.position - transform.position;
-        var forward = transform.forward;
-        var localTarget = transform.InverseTransformPoint(sphere.transform.position);
-
-        angle = Mathf.Atan2(localTarget.x, localTarget.z) * Mathf.Rad2Deg;
-
-        var eulerAngleVelocity : Vector3 = Vector3(0, angle, 0);
-        var deltaRotation : Quaternion = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
-        rigidbody.MoveRotation(rigidbody.rotation * deltaRotation);
-
-        // Work out the direction to target
-        Vector3 direction = target.position - character.position;
-
-        // Check for a zero direction, and make no change if so
-        if (direction.magnitude == 0) {
-            return 0;
-        }
-
-        // Get the naive direction to the target
-        float rotation = Mathf.Atan2(direction.x, direction.z) - character.orientation;
-
-        // Map the result to the (0, 2pi) interval
-        while (rotation > Mathf.PI) {
-            rotation -= 2 * Mathf.PI;
-        }
-        while (rotation < -Mathf.PI) {
-            rotation += 2 * Mathf.PI;
-        }
-        float rotationSize = Mathf.Abs(rotation);
-
-        // Check if we are there, return no steering
-        if (rotationSize < targetRadiusA) {
-            character.rotation = 0;
-        }
-
-        // If we are outside the slowRadius, then use max rotation
-        // Otherwise calculate a scaled rotation
-        float targetRotation = (rotationSize > slowRadiusA ? maxRotation : maxRotation * rotationSize / slowRadiusA);
-
-        // The final target rotation combines speed (already in the variable) and direction
-        targetRotation *= rotation / rotationSize;
-
-        // Acceleration tries to get to the target rotation
-        float angular = targetRotation - character.rotation;
-        angular /= timeToTarget;
-
-        // Check if the acceleration is too great
-        float angularAcceleration = Mathf.Abs(angular);
-        if (angularAcceleration > maxAngularAcceleration) {
-            angular /= angularAcceleration;
-            angular *= maxAngularAcceleration;
-        }
-
-        return angular;
-    }
-
-    // Calculate the Path to follow
-    public Vector3 PathFollow() {
-        Vector3 steering = Vector3.zero;
-        if (current >= Path.Length) {
-            return steering;
-        }
-        if ((Path[current].transform.position - character.position).magnitude > targetRadiusL) {
-            // Get the direction to the target
-            steering = Path[current].transform.position - character.position;
-
-            // Give full acceleration along this direction
-            steering.Normalize();
-            steering *= maxAcceleration;
         } else {
-            current++;
+            float targetSpeed = (distance > slowRadius ? maxSpeed : maxSpeed * distance / slowRadius);
+            Vector3 steering = (direction.normalized * targetSpeed - player.velocity) / timeToTarget;
+            player.AddForce(Vector3.ClampMagnitude(steering, maxAcceleration), ForceMode.VelocityChange);
         }
-        return steering;
     }
 
-    // Calculate the target to face
-    public float pathFace() {
-        if (current >= Path.Length) {
-            return 0;
+    // Face towards the target
+    public void Face(Vector3 target) {
+        Vector3 direction = transform.InverseTransformPoint(target);
+        float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        player.MoveRotation(player.rotation * Quaternion.Euler(new Vector3(0, angle, 0) * Time.deltaTime));
+    }
+    // Face away from the target
+    public void FaceAway(Vector3 target) {
+        Vector3 direction = -transform.InverseTransformPoint(target);
+        float angle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+        player.MoveRotation(player.rotation * Quaternion.Euler(new Vector3(0, angle, 0) * Time.deltaTime));
+    }
+
+    // Follow the path
+    public void PathFollow() {
+        if (current < 0 || Path.Length <= current) {
+            current = 0;
+        } else {
+            if ((Path[current] - player.position).magnitude > targetRadius) {
+                Arrive(Path[current]);
+                Face(Path[current]);
+            } else {
+                current++;
+            }
         }
-        // Work out the direction to target
-        Vector3 direction = Path[current].transform.position - character.position;
-
-        // Check for a zero direction, and make no change if so
-        if (direction.magnitude == 0) {
-            return 0;
-        }
-
-        // Get the naive direction to the target
-        float rotation = Mathf.Atan2(direction.x, direction.z) - character.orientation;
-
-        // Map the result to the (0, 2pi) interval
-        while (rotation > Mathf.PI) {
-            rotation -= 2 * Mathf.PI;
-        }
-        while (rotation < -Mathf.PI) {
-            rotation += 2 * Mathf.PI;
-        }
-        float rotationSize = Mathf.Abs(rotation);
-
-        // Check if we are there, return no steering
-        if (rotationSize < targetRadiusA) {
-            character.rotation = 0;
-        }
-
-        // If we are outside the slowRadius, then use max rotation
-        // Otherwise calculate a scaled rotation
-        float targetRotation = (rotationSize > slowRadiusA ? maxRotation : maxRotation * rotationSize / slowRadiusA);
-
-        // The final target rotation combines speed (already in the variable) and direction
-        targetRotation *= rotation / rotationSize;
-
-        // Acceleration tries to get to the target rotation
-        float angular = targetRotation - character.rotation;
-        angular /= timeToTarget;
-
-        // Check if the acceleration is too great
-        float angularAcceleration = Mathf.Abs(angular);
-        if (angularAcceleration > maxAngularAcceleration) {
-            angular /= angularAcceleration;
-            angular *= maxAngularAcceleration;
-        }
-
-        return angular;
     }
 }
--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -3,66 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 // Stores data for an object used in AI calculations
+[RequireComponent(typeof (AI))]
 public class NPCController : MonoBehaviour {
     // Store variables for objects
-    [HideInInspector]
-    public Rigidbody rb;
-    public GameObject target;
     [SerializeField]
+    private Rigidbody target;
+    public int state = 0;
     private AI ai;
 
-    // Bounds linear changes
-    public float maxSpeedL;
-    public float maxAccelerationL;
-
-    // Bounds angular changes
-    public float maxSpeedA;
-    public float maxAccelerationA;
-
     // On initialization
-    private void Awake() {
-        rb = GetComponent<Rigidbody>();
+    private void Start() {
+        ai = GetComponent<AI>();
     }
-
     // Update the movement
     private void FixedUpdate() {
-        updateLinear(ai.Output(target));
-        updateAngular(ai.Output(target));
-    }
-
-    protected void updateLinear(Steering steering) {
-        // Bound the acceleration
-        steering.linear = Vector2.ClampMagnitude(steering.linear, maxAccelerationL);
-        float angularAcceleration = Mathf.Abs(steering.angular);
-        if (angularAcceleration > maxAccelerationA) {
-            steering.angular /= angularAcceleration;
-            steering.angular *= maxAccelerationA;
+        switch (state) {
+            case 0:
+                ai.Pursue(target.position);
+                ai.Face(target.position);
+                break;
+            case 1:
+                ai.Evade(target.position);
+                ai.FaceAway(target.position);
+                break;
+            case 2:
+                ai.PathFollow();
+                break;
         }
 
-        // Bound the velocity
-        rb.velocity = Vector2.ClampMagnitude(rb.velocity, maxSpeedL);
-        if (rb.angularVelocity > maxSpeedA) {
-            rb.angularVelocity = maxSpeedA;
-        }
-
-        // Update the position and rotation
-        rb.AddForce(velocity - rb.velocity, ForceMode.VelocityChange);
-        rb.angularVelocity += steering.angular;
-    }
-
-    private void update(Vector3 steeringlin, float steeringang, float time) {
-        // Update the orientation, velocity and rotation
-        orientation += rotation * time;
-        velocity += steeringlin * time;
-        rotation += steeringang * time;
-
-        if (velocity.magnitude > maxSpeed) {
-            velocity.Normalize();
-            velocity *= maxSpeed;
-        }
-
-        rb.AddForce(velocity - rb.velocity, ForceMode.VelocityChange);
-        position = rb.position;
-        rb.MoveRotation(Quaternion.Euler(new Vector3(0, Mathf.Rad2Deg * orientation, 0)));
     }
 }
