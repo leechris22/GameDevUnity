baf7ef6fac0271c141f405e69a18f0738f7f64df
Corey D <coreyd2108@gmail.com>
2018-10-29 19:28:42 -0400

    Camera follows and rotates around player
    rotation only works horizontally

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public GameObject player;
+    private Vector3 offset;
+    private int quadrant;
+
+    // Use this for initialization
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        quadrant = 1;
+    }
+
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Move the camera
+        if (Input.GetKey(KeyCode.A)) { offset = Quaternion.AngleAxis(transform.eulerAngles.x * 0.05f, Vector3.up) * offset; }
+        if (Input.GetKey(KeyCode.D)) { offset = Quaternion.AngleAxis(transform.eulerAngles.x * -0.05f, Vector3.up) * offset; }
+        //if (Input.GetKey(KeyCode.W)) { offset = Quaternion.AngleAxis(transform.eulerAngles.x * 0.05f, Vector3.forward) * offset; }
+        //if (Input.GetKey(KeyCode.S)) { offset = Quaternion.AngleAxis(transform.eulerAngles.x * -0.05f, Vector3.forward) * offset; }
+
+        transform.position = player.transform.position + offset;
+        transform.LookAt(player.transform.position);
+    }
+}
