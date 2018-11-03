455f6f5f2873f06fbd974c877ad83bb80c7d50cb
JS <shensjw@users.noreply.github.com>
2018-11-03 12:34:28 -0400

    Add Player jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,18 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-	public float speed;
+	public float speed = 1.0f;
+	public float jumpForce = 1.0f;
 	private Rigidbody rb;
+
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 	}
 
 	void FixedUpdate () {
+		/* Player movement */
 		float moveHorizontal = Input.GetAxis("Horizontal");
 		float moveVertical = Input.GetAxis("Vertical");
-
 		Vector3 movement = new Vector3(moveHorizontal, rb.velocity.y/speed, moveVertical);
 		rb.velocity = movement * speed;
+		// Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
 		// rb.AddForce(movement * speed);
+
+		/* Player jump */
+		Vector3 jump = new Vector3(0.0f, jumpForce, 0.0f);
+		if (Input.GetButtonDown("Jump")) {
+			rb.AddForce(jump, ForceMode.Impulse);
+		}
 	}
 }
