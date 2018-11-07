8aa053326a143a3a5c720fab25d855a5113faf07
Corey D <coreyd2108@gmail.com>
2018-11-07 02:24:44 -0500

    Add Funky Toucan first two waves

--- Assets/Scripts/BossPatterns/FunkyToucan.cs
+++ Assets/Scripts/BossPatterns/FunkyToucan.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyPattern : MonoBehaviour {
+public class FunkyToucan : MonoBehaviour
+{
     Animator anim;
     [SerializeField]
     protected GameObject noteprefab;
@@ -14,16 +15,21 @@ public class EnemyPattern : MonoBehaviour {
     AudioSource[] music;
     AudioSource activeMusic;
     float pt1_rate;
+    float pt1_rate2;
     float pt2_rate;
     float pt3_rate;
-    bool firstAttack=false;
-    bool secondAttack=false;
+    bool firstAttack = false;
+    bool secondAttack = false;
 
-    private void Start() {
+    private void Start()
+    {
         phase = 1;
         activeMusic = null;
         music = GetComponents<AudioSource>();
         Invoke("firstLayer", 3);
+        Invoke("firstLayer2", 3.63f);
+        Invoke("firstLayer2", 4.74f);
+        Invoke("firstLayer2", 5.12f);
         Invoke("startMusic", 3);
         enemyMaxHealth = GetComponent<EnemyHealth>().getMaxHealth();
         anim = GetComponentInChildren<Animator>();
@@ -56,33 +62,49 @@ public class EnemyPattern : MonoBehaviour {
             secondAttack = false;
         }
         if (activeMusic != null) { print(activeMusic.time); }
-        if (enemyHealth <= (enemyMaxHealth * 2 / 3) && activeMusic != null && activeMusic.time >= 13.69f && phase == 1) {
+        if (enemyHealth <= (enemyMaxHealth * 2 / 3) && activeMusic != null && activeMusic.time >= 5.99f && phase == 1)
+        {
             phase = 2;
             startMusic();
             secondLayer();
-            Invoke("secondLayer", 0.1f);
-            Invoke("secondLayer", 0.2f);
-        } else if (enemyHealth <= (enemyMaxHealth / 3) && activeMusic != null && activeMusic.time >= 13.69f && phase == 2) {
+            Invoke("secondLayer", 0.36f);
+            Invoke("secondLayer", 0.74f);
+            Invoke("secondLayer2", 0.95f);
+            Invoke("secondLayer3", 1.35f);
+            Invoke("secondLayer3", 1.73f);
+            Invoke("secondLayer4", 2.27f);
+            Invoke("secondLayer4", 2.62f);
+        }
+        else if (enemyHealth <= (enemyMaxHealth / 3) && activeMusic != null && activeMusic.time >= 5.99f && phase == 2)
+        {
             phase = 3;
             thirdLayer();
-            for (float i = 1; i < 20; i++) {
+            for (float i = 1; i < 20; i++)
+            {
                 Invoke("thirdLayer", i / 10f);
             }
             startMusic();
         }
     }
 
-    private void startMusic() {
-        if (phase == 1) {
+    private void startMusic()
+    {
+        if (phase == 1)
+        {
             music[0].Play();
             activeMusic = music[0];
-            pt1_rate = music[0].clip.length / 8f;
-        } else if (phase == 2) {
+            pt1_rate = music[0].clip.length / 24f;
+            pt1_rate2 = music[0].clip.length / 2f;
+        }
+        else if (phase == 2)
+        {
             music[0].Stop();
             music[1].Play();
             activeMusic = music[1];
-            pt2_rate = music[1].clip.length / 8f;
-        } else if (phase == 3) {
+            pt2_rate = music[1].clip.length / 2f;
+        }
+        else if (phase == 3)
+        {
             music[1].Stop();
             music[2].Play();
             activeMusic = music[2];
@@ -90,17 +112,46 @@ public class EnemyPattern : MonoBehaviour {
         }
     }
 
-    private void firstLayer() {
-        print("start first layer");
+    private void firstLayer()
+    {
         attack.Shoot(-1);
         firstAttack = true;
-        if (phase == 1) { Invoke("firstLayer", pt1_rate); }
+        Invoke("firstLayer", pt1_rate);
     }
 
-    private void secondLayer() {
+    private void firstLayer2()
+    {
         attack.Shockwave(50);
         secondAttack = true;
+        if (phase == 1) { Invoke("firstLayer2", pt1_rate2); }
+    }
+
+    private void secondLayer()
+    {
+        attack.Wall(50, 47);
+        secondAttack = true;
         Invoke("secondLayer", pt2_rate);
+    }
+
+    private void secondLayer2()
+    {
+        attack.Wall(50, 1);
+        secondAttack = true;
+        Invoke("secondLayer2", pt2_rate);
+    }
+
+    private void secondLayer3()
+    {
+        attack.Wall(50, 4);
+        secondAttack = true;
+        Invoke("secondLayer3", pt2_rate);
+    }
+
+    private void secondLayer4()
+    {
+        attack.Shockwave(50);
+        secondAttack = true;
+        Invoke("secondLayer4", pt2_rate);
     }
 
     private void thirdLayer()
